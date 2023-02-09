fff9ef3005fddb50d89b54e7d58614722cf2c700
qkransrb <qkransrb90@gmail.com>
2023-02-09 18:26:16 +0900

    #12 - Auditing Fields 추출
    createdAt, createdBy, modifiedAt, modifiedBy 추출
    도메인과 직접적인 연관이 없는 필드를 따로 추출하여 관리
    `@MappedSuperclass` 를 이용하여 상속 방식으로 추출
    `@DateTimeFormat` 어노테이션 추가, JPA 옵션 개선

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,14 +1,8 @@
 package com.fastcampus.projectboard.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -22,11 +16,10 @@ import java.util.Set;
                 @Index(columnList = "createdBy")
         }
 )
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Article {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,22 +40,6 @@ public class Article {
     @OrderBy("id")
     @ToString.Exclude
     private final Set<Comment> comments = new LinkedHashSet<>();
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
